0794be6785c60e3fffb890ac6e0520bcd5c55234
elina <elinapark0818@gmail.com>
2022-12-01 02:45:19 +0900

    feat: portalmodal

---
+++ src/components/modal/Modal.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Modal.css";
+
+export const Modal = ({ onClose }) => {
+  return (
+    <div className="modal_container">
+      <div className="modal_content">
+        <h1>test</h1>
+      </div>
+    </div>
+  );
+};
