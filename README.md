49bb3f0ce5259d416bfff215581e523749bbde51
elina <elinapark0818@gmail.com>
2022-12-03 19:07:11 +0900

    fix: yarn version update & add css lib

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
   - PortalModal
 - React-router-dom
 - Recoil
+- Sass > scss
 
 ## 디렉토리 구조
 
@@ -15,11 +16,21 @@
     - images
   - components
     - modal
+      - Portal
     - navbar
   - page
-    - main (skill)
+    - main
+      - 프로필사진 및 간단 소개 (인스타그램 피드처럼)
+      - 프로필 이미지 캐로셀 적용
     - about
+      - 스킬셋
     - projects
+      - 갤러리 형태
+      - 클릭 시, 큰 모달
     - contact
   - route
     - appRoute
+
+## 배포
+
+(예정) vercel
--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 import { RecoilRoot } from "recoil";
 import { BrowserRouter } from "react-router-dom";
 import { AppRoute } from "./route/AppRoute";
-import { Navbar } from "./components/navbar/Navbar";
-import { Footer } from "./components/footer/Footer";
+// import { Navbar } from "./components/navbar/Navbar";
+// import { Footer } from "./components/footer/Footer";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -12,9 +12,7 @@ root.render(
   <BrowserRouter>
     <RecoilRoot>
       <Suspense>
-        <Navbar />
         <AppRoute />
-        <Footer />
       </Suspense>
     </RecoilRoot>
   </BrowserRouter>
